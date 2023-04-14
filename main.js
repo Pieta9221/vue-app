d9c6a46c0b05a303cc30c905c52b5994433c6992
Pieta9221 <mercy_nwaodu@yahoo.com>
2023-04-14 16:52:26 +0100

    update

--- main.js
+++ main.js
@@ -1,13 +1,33 @@
 const app = Vue.createApp({
     data(){
         return {
+            cart: 0,
             product: 'Boots',
             description: 'A suitable product for the rainy season and beyond',
             image: './assets/images/socks_blue.jpg',
             url: 'https://google.com',
-            inStock: true, 
+            inStock: fals, 
             inventory: 10,
-            onSale: true
+            onSale: true,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                {id: 2234, color: 'green', image: './assets/images/socks_green.jpg'},
+                {id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg'},
+            ],
+            sizes: ['L', 'M', 'S']
+        }
+    },
+    methods: {
+        addToCart(){
+            this.cart += 1
+        },
+        removeFromCart(){
+            if (this.cart >= 1) {
+                this.cart -= 1
+            }
+        },
+        updateImage(variantImage){
+            this.image = variantImage
         }
     }
 })
