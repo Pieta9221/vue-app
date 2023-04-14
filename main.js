4d2cac014544f1c5352c0baec2aadabf9735494c
Pieta9221 <mercy_nwaodu@yahoo.com>
2023-04-14 13:10:21 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+const app = Vue.createApp({
+    data(){
+        return {
+            product: 'Boots',
+            description: 'A suitable product for the rainy season and beyond',
+            image: './assets/images/socks_blue.jpg',
+            url: 'https://google.com',
+            inStock: false
+        }
+    }
+})
