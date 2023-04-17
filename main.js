b784d4c2767e1e05cf8f9651c4aea01ac38ffa9d
Pieta9221 <mercy_nwaodu@yahoo.com>
2023-04-17 14:16:39 +0100

    components and form

--- main.js
+++ main.js
@@ -1,33 +1,19 @@
 const app = Vue.createApp({
     data(){
         return {
-            cart: 0,
-            product: 'Boots',
-            description: 'A suitable product for the rainy season and beyond',
-            image: './assets/images/socks_blue.jpg',
-            url: 'https://google.com',
-            inStock: fals, 
-            inventory: 10,
-            onSale: true,
-            details: ['50% cotton', '30% wool', '20% polyester'],
-            variants: [
-                {id: 2234, color: 'green', image: './assets/images/socks_green.jpg'},
-                {id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg'},
-            ],
-            sizes: ['L', 'M', 'S']
+            cart: [],
+            premium: true
         }
     },
     methods: {
-        addToCart(){
-            this.cart += 1
+        updateCart(id){
+            this.cart.push(id)
         },
-        removeFromCart(){
-            if (this.cart >= 1) {
-                this.cart -= 1
-            }
+        removeItem(id){
+            const index = this.cart.indexOf(id)
+                if (index > -1) {
+                    this.cart.splice(index, 1)
+               }
         },
-        updateImage(variantImage){
-            this.image = variantImage
-        }
     }
 })
